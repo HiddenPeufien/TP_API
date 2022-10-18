4374c8f80ee2b5bac35c0219467bc595814662f6
HiddenPeufien <damien.marchat@epfedu.fr>
2022-10-18 11:57:16 +0200

    premier commit

---
+++ GithubRequest.py
@@ -0,0 +1,10 @@
+class GithubRequest:
+
+    _base_url = "https://api.github.com"
+
+    @classmethod
+    def get_events(cls):
+        response = requests.get(cls._base_url + "/events")
+
+        return Response(status_code=response.status_code, content=response.json())
+
---
+++ VersioningEvent.py
@@ -0,0 +1,7 @@
+class VersioningEvent:
+
+    def __init__(self, type, message, created_at):
+        self.type = type
+        self.message = message
+        self.created_at = created_at
+
